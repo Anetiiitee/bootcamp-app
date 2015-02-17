c8b1c575ae45e3584ae091b1b31f9a55eab321d0
Student <anete.gobleja@gmail.com>
2015-02-17 16:04:20 +0200

    pievienoju stylesheet

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 <html>
   <head>
   <meta charset="utf-8">
+  <link rel="stylesheet"href="stle.css">
     <title>This is a title</title>
   </head>
   <body>
-    <p>Hello world!</p>
+    <h1>Hello world!</h1>
   <form action="" method="post">
   <label for="name">Name</label>
   <input type="text" id="name" name="name">
   <button type="submit">ok</button>
 </form>
-<a href="second.php">
-  <a href="http://accessibility.psu.edu/imageshtml"alt="links">
-  <IMG SRC="http://media.veryfunnypics.eu/2014/01/funny-pics-baby-mirror-your-hair-looks-funny.jpg" alt="Bilde">
+  <a href="http://accessibility.psu.edu/imageshtml">Links</a>
+  <IMG SRC="http://media.veryfunnypics.eu/2014/01/funny-pics-baby-mirror-your-hair-looks-funny.jpg"alt="Bilde">
+  </img>
   </body>
 </html>
