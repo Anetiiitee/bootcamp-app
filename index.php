373b6add9d277e8e9f282cf1c765ed879923b4c8
Student <anete.gobleja@gmail.com>
2015-02-17 14:55:29 +0200

    Pievienoju bildi

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-Hello world!
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+    <title>This is a title</title>
+  </head>
+  <body>
+    <p>Hello world!</p>
+  <form action="" method="post">
+  <label for="name">Name</label>
+  <input type="text" id="name" name="name">
+  <button type="submit">ok</button>
+</form>
+<a href="second.php">
+  <a href="http://accessibility.psu.edu/imageshtml"alt="links">
+  <IMG SRC="http://media.veryfunnypics.eu/2014/01/funny-pics-baby-mirror-your-hair-looks-funny.jpg" alt="Bilde">
+  </body>
+</html>
