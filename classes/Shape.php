f095b72232ea12729d0aef1234a78493f04feeeb
Student <anete.gobleja@gmail.com>
2015-02-19 17:39:47 +0200

    Figuuras

---
+++ classes/Shape.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bootcamp\Demo;
+
+/**
+* Gets area of shape
+*/
+abstract class Shape {
+  /**
+  *Gets area of shape
+  * @return float shape area
+  */
+  public abstract function getArea();
+
+}
