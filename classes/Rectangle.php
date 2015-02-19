3e067c5a51db6ccd09f4b202307e9147d6c9071a
Student <anete.gobleja@gmail.com>
2015-02-19 17:39:17 +0200

    Taisnsturis

---
+++ classes/Rectangle.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bootcamp\Demo;
+
+class Rectangle extends Shape {
+ /**
+  * @param $width float width
+  * @param $height float height
+  */
+  public $width;
+  public $height;
+  /**
+  * Creates an rectangle instance
+  * @param $width float width
+  * @param $height float height
+  * @return rectangular area
+  */
+  public function getArea()
+  {
+    return $this->width*$this->height;
+  }
+  /**
+  * @param $width float Rectangular width
+  * @param $height float Rectangular height
+  * @return void
+  */
+  public function __construct($width,$heigth) {
+    $this->width = $width;
+    $this->height=$heigth;
+  }
+}
